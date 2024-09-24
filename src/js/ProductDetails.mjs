f539173f59fa7adbfb543ef173b6a1404a0af7c2
VictoryChibueze <alhajivicci12@gmail.com>
2024-09-24 18:31:53 +0100

    vc team 2

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -9,7 +9,7 @@ export default class ProductDetails {
 
   async init() {
     // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-    await this.dataSource;
+    this.product = await this.dataSource.findProductById(this.productId);
     // once we have the product details we can render out the HTML
     // once the HTML is rendered we can add a listener to Add to Cart button
     // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
