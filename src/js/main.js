f6d6ec2dfb1c430594d79f88726c4256a13b361a
MiriamCall <bow18005@byui.edu>
2024-09-25 12:37:58 -0600

    Refactor main.js and add ProductList class
    This commit refactors the main.js file by adding an import statement for the ProductList class. It also creates an instance of the ProductList class with the "tents" category, the dataSource object, and the "productList" element. This change improves the organization and functionality of the code.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+
+import ProductListing from "./ProductList.mjs";
+
+const productList = new ProductListing("tents", dataSource, document.getElementById("productList"));
