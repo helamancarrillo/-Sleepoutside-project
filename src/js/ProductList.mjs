7d17f6c12bfd970d7d61b27ec2cf829ab6410038
heathergibb <hrgibb@gmail.com>
2024-09-25 19:35:43 -0600

    initial work on dynamic product list

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,33 @@
+// import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product) {
+    return `<li class="product-card">
+        <a href="product_pages/index.html?product=${product.Id}">
+              <img
+                src=${product.Image}
+                alt="Image of ${product.Name}"
+              />
+              <h3 class="card__brand">${product.Brand.Name}</h3>
+              <h2 class="card__name">${product.NameWithoutBrand}</h2>
+              <p class="product-card__price">$${product.FinalPrice}</p></a>
+          </li>`;
+}
+
+export default class ProductListing {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+        // render the list
+        this.renderList(list);
+    }
+    renderList(list) {
+        const htmlStrings = list.map(productCardTemplate);  
+        this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+        // renderListWithTemplate(...);
+    }
+}
