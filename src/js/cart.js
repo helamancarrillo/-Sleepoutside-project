b730e43760a600afb48ae4ecc9fa65d8c517017a
MiriamCall <bow18005@byui.edu>
2024-09-27 23:09:42 -0600

    Refactor cart footer and item count display
    - Updated the HTML elements in the cart index.html file to include a new class for the cart footer and item count.
    - Added CSS styles to hide the cart footer initially.
    - Modified the JavaScript code in cart.js to dynamically render the cart footer and display the total item count.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,41 @@
 import { getLocalStorage } from "./utils.mjs";
 
-function renderCartContents() {
-  const cartItems = getLocalStorage("addToCart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-}
+document.addEventListener("DOMContentLoaded", () => {
+  function renderCartContents() {
+    const cartItems = getLocalStorage("addToCart");
 
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+    function cartItemTemplate(item) {
+      return `<li class="cart-card divider">
+        <a href="#" class="cart-card__image">
+          <img
+            src="${item.Image}"
+            alt="${item.Name}"
+          />
+        </a>
+        <a href="#">
+          <h2 class="card__name">${item.Name}</h2>
+        </a>
+        <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+        <p class="cart-card__quantity">qty: 1</p>
+        <p class="cart-card__price">$${item.FinalPrice}</p>
+      </li>`;
+    }
 
-  return newItem;
-}
+    if (cartItems.length > 0) {
+      const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+      document.querySelector(".product-list").innerHTML = htmlItems.join("");
 
-renderCartContents();
+      const cartTotal = cartItems.reduce((total, item) => total + item.FinalPrice, 0);
+
+      // Show the cart footer element
+      const cartFooterElement = document.querySelector(".cart-footer");
+      cartFooterElement.classList.remove("total-hide");
+      cartFooterElement.querySelector(".item-count").innerHTML = `Items: ${cartItems.length}`;
+      cartFooterElement.querySelector(".cart-total").innerHTML = `Total: $${cartTotal}`;
+    } else {
+      document.querySelector(".product-list").innerHTML = `<p class="empty-cart-message">Your cart is empty</p>`;
+    }
+  }
+
+  renderCartContents();
+});
