a23f0d2e9963320c854b3b85d0596461769a5cd7
MiriamCall <bow18005@byui.edu>
2024-09-27 22:39:50 -0600

    Refactor product listing and main script
    - Remove unused product cards from the product listing in index.html
    - Update the script source in index.html to use main.js instead of product.js
    - Refactor the ProductList class in ProductList.mjs to use a template function for rendering product cards
    - Add a new renderListWithTemplate function in utils.mjs to render a list of products using a template function
    - Update main.js to import and use the updated ProductList class and render the product list

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
 import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const listing = new ProductList("Tents", dataSource, element);
 
-import ProductListing from "./ProductList.mjs";
-
-const productList = new ProductListing("tents", dataSource, document.getElementById("productList"));
+listing.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -15,6 +15,18 @@ export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
 
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(templateFn);
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
+
+}
+
+
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
