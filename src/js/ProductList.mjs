07a7e72e4416d6bc4f09b2a198375a5074252786
VictoryChibueze <alhajivicci12@gmail.com>
2024-10-01 04:12:01 +0100

    task report

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,37 +1,64 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
-function productCardTemplate(product) {
-    return `<li class="product-card">
-        <a href="product_pages/index.html?product=${product.Id}">
-              <img
-                src=${product.Image}
-                alt="Image of ${product.Name}"
-              />
-              <h3 class="card__brand">${product.Brand.Name}</h3>
-              <h2 class="card__name">${product.NameWithoutBrand}</h2>
-              <p class="product-card__price">$${product.FinalPrice}</p></a>
-          </li>`;
+function productListTemplate(product) {
+  return `<li class="product-card">
+    <a href="product_pages/index.html?product=${product.Id}">
+      <img
+        src="${product.Image}"
+        alt="Image of ${product.Name}"
+      />
+      <h3 class="card__brand">${product.Brand.Name}</h3>
+      <h2 class="card__name">${product.Name}</h2>
+      <p class="product-card__price">$${product.FinalPrice}</p>
+    </a>
+  </li>`;
 }
 
 export default class ProductListing {
-    constructor(category, dataSource, listElement) {
-        this.category = category;
-        this.dataSource = dataSource;
-        this.listElement = listElement;
-    }
+  // constructor housing the category ,dataSource and listElement of the Productlisting class
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
 
-    async init() {
-        const list = await this.dataSource.getData();
+  async init() {
+    const list = await this.dataSource.getData();
+    const listToRender = this.filterProductList(list);
+    console.log(list);
+    this.renderList(listToRender);
+  }
 
-        // reduce product list to first 4 products
-        let newList = list.filter((product, i) => i < 4);
+  // renderList(list) {
+  //   const htmlString = list.map(productListTemplate);
+  //   this.listElement.insertAdjacentHTML("afterbegin", htmlString.join(""));
+  // }
+  // filterProductList(list) {
+  //   list.filter((el) => el.product.Name);
+  // }
 
-        // render the list
-        renderListWithTemplate(productCardTemplate, this.listElement, newList, "afterbegin", false);
-    }
+  // Get the first 4 unique products
 
-    // renderList(list) {
-    //     const htmlStrings = list.map(productCardTemplate);  
-    //     this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
-    // }
+  filterProductList(list) {
+    const uniqueProducts = list.filter(
+      (list, index, self) =>
+        index ===
+        self.findIndex(
+          (el) =>
+            el.NameWithoutBrand.split("-")[0] ===
+            list.NameWithoutBrand.split("-")[0],
+        ),
+    );
+
+    return uniqueProducts;
+  }
+
+  renderList(list) {
+    renderListWithTemplate(
+      productListTemplate,
+      this.listElement,
+      list,
+      "afterbegin",
+    );
+  }
 }
