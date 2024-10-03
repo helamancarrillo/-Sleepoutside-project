6a5ac8e15b4ffe93bd1c0132958da89e359b07c0
MiriamCall <bow18005@byui.edu>
2024-10-03 15:17:07 -0600

    Refactor ProductData constructor to remove category parameter

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,9 +9,7 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
+  constructor() {
   }
 
   async getData(category) {
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -3,6 +3,7 @@ import ProductListing from "./ProductList.mjs";
 
 const tentData = new ProductData("tents");
 const element = document.querySelector(".product-list");
-const list = new ProductListing("tents", tentData, element);
+// const list = new ProductListing("tents", tentData, element);
 
+const list = await this.dataSource.getData(this.category);
 list.init();
