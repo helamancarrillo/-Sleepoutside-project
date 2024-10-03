3540089651365942b984527ea04111664cff0a82
helamancarrillo <helamancarrillo11@gmail.com>
2024-10-02 21:26:31 -0600

    team activity

---
+++ week3-practice/fetch.js
@@ -0,0 +1,27 @@
+
+
+const url = "https://pokeapi.co/api/v2/pokemon";
+let results = null;
+async function getPokemon(url) {
+  const response = await fetch(url);
+  //check to see if the fetch was successful
+  if (response.ok) {
+    // the API will send us JSON...but we have to convert the response before we can use it
+    // .json() also returns a promise...so we await it as well.
+    const data = await response.json();
+    doStuff(data);
+  }
+}
+
+function doStuff(data) {
+    results = data;
+    console.log('first: ', results);
+    results.results.forEach((pokemon) => {
+    const div = document.createElement('div');
+    div.textContent = pokemon.name;
+    document.querySelector('main').appendChild(div)
+    });
+}
+
+getPokemon(url);
+console.log("second: ", results);
