3a82dda7fc04ed8f5b0d4a29a64395e0c7f7d35a
MiriamCall <bow18005@byui.edu>
2024-10-09 14:39:18 -0600

    Refactor checkout page and checkout process
    - Removed unnecessary paragraphs for cart total, shipping estimate, tax, and order total in the checkout page.
    - Added a submit button for the checkout form.
    - Updated the calculateItemSummary() method in the checkout process to correctly calculate the total amount of items in the cart.
    - Updated the calculateOrdertotal() method in the checkout process to calculate the shipping, tax, and order total correctly.
    - Updated the displayOrderTotals() method in the checkout process to display the updated order totals.

--- src/js/checkoutProcess.mjs
+++ src/js/checkoutProcess.mjs
@@ -18,14 +18,22 @@ export default class CheckoutProcess {
   
     calculateItemSummary() {
       // calculate and display the total amount of the items in the cart, and the number of items.
+        this.itemTotal = this.list.reduce((total, item) => total + item.finalPrice, 0);
+        
   
     }
   
     calculateOrdertotal() {
       // calculate the shipping and tax amounts. Then use them to along with the cart total to figure out the order total
-  
-      // display the totals.
-      this.displayOrderTotals();
+      this.shipping = 10 + (this.list.length - 1) * 2;
+      this.tax = (this.itemTotal * 0.06).toFixed(2);
+      this.orderTotal = (
+        parseFloat(this.itemTotal) +
+        parseFloat(this.shipping) +
+        parseFloat(this.tax)
+        ).toFixed(2);
+        this.displayOrderTotals();
+
     }
   
     displayOrderTotals() {
